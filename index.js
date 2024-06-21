d0ba173fdaad0671d354929fa4f1d26109311698
Affan <aafhanjaved@gmail.com>
2024-06-22 02:49:41 +0500

    vercel setup 2

--- index.js
+++ index.js
@@ -23,3 +23,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
