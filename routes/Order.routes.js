87e449fa39f724f4aade3038bd003d8f638ef4ed
Affan <aafhanjaved@gmail.com>
2024-07-01 21:41:54 +0500

    trying post

--- routes/Order.routes.js
+++ routes/Order.routes.js
@@ -2,7 +2,7 @@ import express from "express";
 const router = express.Router();
 import OrderController from "../controllers/Order.controller.js";
 router.post("/createorder", OrderController.createOrder);
-router.get("/geteaterorder", OrderController.geteaterOrders);
+router.post("/geteaterorder", OrderController.geteaterOrders);
 router.get("/getproviderorder", OrderController.getProviderOrders);
 router.post("/updateorder", OrderController.updateOrder);
 export default router;
