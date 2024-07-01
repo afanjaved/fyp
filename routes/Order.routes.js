fcb373bfeedb11b0b7433b19aba0126aa95dea50
Affan <aafhanjaved@gmail.com>
2024-07-01 22:44:02 +0500

    post route for chefordrs

--- routes/Order.routes.js
+++ routes/Order.routes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 import OrderController from "../controllers/Order.controller.js";
 router.post("/createorder", OrderController.createOrder);
 router.post("/geteaterorder", OrderController.geteaterOrders);
-router.get("/getproviderorder", OrderController.getProviderOrders);
+router.post("/getproviderorder", OrderController.getProviderOrders);
 router.post("/updateorder", OrderController.updateOrder);
 export default router;
